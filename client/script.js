0738d0b7e4a5e91b72eee49a2687afd8aebffcef
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-16 16:53:15 +0800

    renamed designated folders for client

--- client/script.js
+++ client/script.js
@@ -1,8 +1,8 @@
-let role = document.getElementById('role')
-let email = document.getElementById('email')
-let password = document.getElementById('password')
-let loginButton = document.getElementById('login')
+let role = document.getElementById("role");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let loginButton = document.getElementById("login");
 
-loginButton.addEventListener('click', () => {
-    
-})
+loginButton.addEventListener("click", () => {
+  console.log(role.value, email.value, password.value);
+});
