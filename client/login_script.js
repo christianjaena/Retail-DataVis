07261ee289920230b06d9f76e697a1a5818f2142
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-17 15:23:58 +0800

    provided validation for login

--- client/login_script.js
+++ client/login_script.js
@@ -9,41 +9,55 @@ signupButton.addEventListener('click', () => {
 });
 
 loginButton.addEventListener('click', async () => {
-  let data = {
-    email: email.value,
-    password: password.value,
-    role: role.value,
-  };
+  if (validateInput(email.value, password.value, role.value)) {
+    let data = {
+      email: email.value,
+      password: password.value,
+      role: role.value,
+    };
 
-  let response = await fetch('http://localhost:5000/user/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
+    let response = await fetch('http://localhost:5000/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-  let result = await response.json();
-  if (result !== 'No such user found') {
-    switch (result.Role) {
-      case 'SuperAdmin':
-        window.location.href = 'http://localhost:5000/dashboard';
-        window.alert(`Logged in as ${result.Email}`);
-        break;
-      case 'InventoryManager':
-        window.location.href = 'http://localhost:5000/inventory';
-        window.alert(`Logged in as ${result.Email}`);
-        break;
-      case 'Finance':
-        window.location.href = 'http://localhost:5000/finance';
-        window.alert(`Logged in as ${result.Email}`);
-        break;
-      case 'Customer':
-        window.location.href = 'http://localhost:5000/ecommerce';
-        window.alert(`Logged in as ${result.Email}`);
-        break;
+    let result = await response.json();
+    if (result !== 'No such user found') {
+      switch (result.Role) {
+        case 'SuperAdmin':
+          window.location.href = 'http://localhost:5000/dashboard';
+          window.alert(`Logged in as ${result.Email}`);
+          break;
+        case 'InventoryManager':
+          window.location.href = 'http://localhost:5000/inventory';
+          window.alert(`Logged in as ${result.Email}`);
+          break;
+        case 'Finance':
+          window.location.href = 'http://localhost:5000/finance';
+          window.alert(`Logged in as ${result.Email}`);
+          break;
+        case 'Customer':
+          window.location.href = 'http://localhost:5000/ecommerce';
+          window.alert(`Logged in as ${result.Email}`);
+          break;
+      }
+    } else {
+      window.alert('No such user found');
     }
   } else {
-    window.alert('No such user found');
+    window.alert('Please provide proper credentials.');
   }
 });
+
+function validateInput(email, password) {
+  const emailRegex = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i;
+  const isValidEmail = email.search(emailRegex);
+  if (isValidEmail === -1 || email === '' || password === '' || role === '') {
+    return false;
+  } else {
+    return true;
+  }
+}
