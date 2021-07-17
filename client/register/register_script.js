528b8e20c40cb6a240b00277424a00de3dd074cf
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-17 14:38:42 +0800

    implemented validation for registration of users

--- client/register/register_script.js
+++ client/register/register_script.js
@@ -4,20 +4,37 @@ let password = document.getElementById('password');
 let registerButton = document.getElementById('register');
 
 registerButton.addEventListener('click', async () => {
-  let data = {
-    email: email.value,
-    password: password.value,
-    role: role.value,
-  };
+  if (validateInput(email.value, password.value)) {
+    let data = {
+      email: email.value,
+      password: password.value,
+      role: role.value,
+    };
 
-  let response = await fetch('http://localhost:5000/user/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
+    let response = await fetch('http://localhost:5000/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-  let result = await response.json();
-  console.log(result);
+    let result = await response.json();
+    window.alert(result);
+    if (result === 'Registered successfully') {
+      window.location.href = 'http://localhost:5000/';
+    }
+  } else {
+    window.alert('Please provide proper credentials');
+  }
 });
+
+function validateInput(email, password) {
+  const emailRegex = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i;
+  const isValidEmail = email.search(emailRegex);
+  if (isValidEmail === -1 || email === '' || password === '') {
+    return false;
+  } else {
+    return true;
+  }
+}
--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -3,11 +3,19 @@ const postgres = require('../../database/postgres');
 const register_user = async (req, res) => {
   try {
     const { email, password, role } = req.body;
-    const newUser = await postgres.query(
-      'INSERT INTO "Users"("Email", "Password", "Role") VALUES($1, $2, $3) RETURNING *',
+    const isUser = await postgres.query(
+      'SELECT * FROM "Users" WHERE "Email"=$1 AND "Password"=$2 AND "Role"=$3',
       [email, password, role]
     );
-    res.json(newUser.rows[0]);
+    if (isUser.rows[0]) {
+      res.status(400).json('User exists');
+    } else {
+      const newUser = await postgres.query(
+        'INSERT INTO "Users"("Email", "Password", "Role") VALUES($1, $2, $3) RETURNING *',
+        [email, password, role]
+      );
+      res.status(200).json('Registered successfully');
+    }
   } catch (err) {
     res.status(400).json(err.message);
     console.log(err.message);
@@ -22,9 +30,9 @@ const login_user = async (req, res) => {
       [email, password, role]
     );
     if (isUser.rows[0]) {
-      res.status(200).json(isUser.rows[0])
+      res.status(200).json(isUser.rows[0]);
     } else {
-      res.status(400).json("No such user found")
+      res.status(400).json('No such user found');
     }
   } catch (err) {
     res.status(400).json(err.message);
