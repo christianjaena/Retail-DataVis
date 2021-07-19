ebffd3754855175ec485de0a596320e46e3b853f
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-19 15:46:03 +0800

    added api endpoint for daily, weekly, monthly, and annual sales data

--- client/dashboard/dashboard_script.js
+++ client/dashboard/dashboard_script.js
@@ -1,64 +1,58 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-let logoutButton = document.getElementById("logout");
+let sidebar = document.querySelector('.sidebar');
+let closeBtn = document.querySelector('#btn');
+let searchBtn = document.querySelector('.bx-search');
+let logoutButton = document.getElementById('logout');
 
-logoutButton.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/";
-  localStorage.removeItem("user");
+logoutButton.addEventListener('click', () => {
+  window.location.href = 'http://localhost:5000/';
+  localStorage.removeItem('user');
 });
 
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
   menuBtnChange(); //calling the function(optional)
 });
 
-searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener('click', () => {
   // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
+  sidebar.classList.toggle('open');
   menuBtnChange(); //calling the function(optional)
 });
 
 function menuBtnChange() {
-  if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+  if (sidebar.classList.contains('open')) {
+    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the iocns class
   } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu'); //replacing the iocns class
   }
 }
 
-let shop = document.getElementById("shop");
-let inventory = document.getElementById("inventory");
-let user = document.getElementById("user");
-let finance = document.getElementById("finance");
+let shop = document.getElementById('shop');
+let inventory = document.getElementById('inventory');
+let user = document.getElementById('user');
+let finance = document.getElementById('finance');
 
-shop.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/ecommerce";
+shop.addEventListener('click', () => {
+  window.location.href = 'http://localhost:5000/ecommerce';
 });
-inventory.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/inventory";
+inventory.addEventListener('click', () => {
+  window.location.href = 'http://localhost:5000/inventory';
 });
-user.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/user";
+user.addEventListener('click', () => {
+  window.location.href = 'http://localhost:5000/user';
 });
-finance.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/finance";
-});
-let logoutButton = document.getElementById("logout");
-
-logoutButton.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/";
-  localStorage.removeItem("user");
+finance.addEventListener('click', () => {
+  window.location.href = 'http://localhost:5000/finance';
 });
 
 function checkUser() {
   if (
-    !localStorage.getItem("user") ||
-    JSON.parse(localStorage.getItem("user")).Role === "Customer" ||
-    JSON.parse(localStorage.getItem("user")).Role === "InventoryManager" ||
-    JSON.parse(localStorage.getItem("user")).Role === "Finance"
+    !localStorage.getItem('user') ||
+    JSON.parse(localStorage.getItem('user')).Role === 'Customer' ||
+    JSON.parse(localStorage.getItem('user')).Role === 'InventoryManager' ||
+    JSON.parse(localStorage.getItem('user')).Role === 'Finance'
   ) {
-    window.alert("You are not authorized to view this page");
-    window.location.href = "http://localhost:5000/";
+    window.alert('You are not authorized to view this page');
+    window.location.href = 'http://localhost:5000/';
   }
 }
