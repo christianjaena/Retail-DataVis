2985acbe31e11ec7438f2718f6d556a4a55953e1
Domerck Brigson Quiton <domerckbrigson.quiton@msugensan.edu.ph>
2021-07-20 17:34:45 +0800

    sidebar format and access changes

--- client/inventory/inventory_script.js
+++ client/inventory/inventory_script.js
@@ -1,14 +1,15 @@
-let sidebar = document.querySelector('.sidebar');
-let closeBtn = document.querySelector('#btn');
-let searchBtn = document.querySelector('.bx-search');
-let logoutButton = document.getElementById('logout');
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+let logoutButton = document.getElementById("logout");
 
-logoutButton.addEventListener('click', () => {
-  window.location.href = 'http://localhost:5000/';
-  localStorage.removeItem('user');
+logoutButton.addEventListener("click", () => {
+  window.location.href = "http://localhost:5000/";
+  localStorage.removeItem("user");
 });
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
   menuBtnChange(); //calling the function(optional)
 });
 
@@ -18,66 +19,21 @@ searchBtn.addEventListener("click", () => {
   menuBtnChange(); //calling the function(optional)
 });
 
-// following are the code to change sidebar button(optional)
 function menuBtnChange() {
-  if (sidebar.classList.contains('open')) {
-    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the iocns class
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
   } else {
-    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu'); //replacing the iocns class
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
   }
 }
 
 function checkUser() {
   if (
-    !localStorage.getItem('user') ||
-    JSON.parse(localStorage.getItem('user')).Role === 'Customer' ||
-    JSON.parse(localStorage.getItem('user')).Role === 'Finance'
+    !localStorage.getItem("user") ||
+    JSON.parse(localStorage.getItem("user")).Role === "Customer" ||
+    JSON.parse(localStorage.getItem("user")).Role === "InventoryManager"
   ) {
-    window.alert('You are not authorized to view this page');
-    window.location.href = 'http://localhost:5000/';
+    window.alert("You are not authorized to view this page");
+    window.location.href = "http://localhost:5000/";
   }
 }
-var width = 450;
-height = 450;
-margin = 40;
-
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin;
-
-// append the svg object to the div called 'my_dataviz'
-var svg = d3
-  .select('#my_dataviz')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-
-// Create dummy data
-var data = { a: 9, b: 20, c: 30, d: 8, e: 12 };
-
-// set the color scale
-var color = d3
-  .scaleOrdinal()
-  .domain(data)
-  .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56']);
-
-// Compute the position of each group on the pie:
-var pie = d3.pie().value(function (d) {
-  return d.value;
-});
-var data_ready = pie(d3.entries(data));
-
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc().innerRadius(0).outerRadius(radius))
-  .attr('fill', function (d) {
-    return color(d.data.key);
-  })
-  .attr('stroke', 'black')
-  .style('stroke-width', '2px')
-  .style('opacity', 0.7);
