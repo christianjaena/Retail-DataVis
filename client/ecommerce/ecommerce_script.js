c8e5f0806b17b3713fded1f3847d64d0eafadec4
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-21 15:48:33 +0800

    changed ecommerce css flexbox to grid

--- client/ecommerce/ecommerce_script.js
+++ client/ecommerce/ecommerce_script.js
@@ -1,35 +1,41 @@
 let shopCartDiv = document.getElementById('shop-cart');
 let content = document.getElementById('content');
 let logoutButton = document.getElementById('logout');
+let products = document.getElementById('products');
 
 logoutButton.addEventListener('click', () => {
   window.location.href = 'http://localhost:5000/';
   localStorage.removeItem('user');
 });
 
-shopButton.addEventListener('click', async () => {
-  content.innerHTML = '';
+(async function getProducts() {
   let response = await fetch('http://localhost:5000/retail/items');
   let data = await response.json();
 
   data.forEach((item, idx) => {
-    content.innerHTML += `
-    <div>
-        <li id="${idx}">${item.Description} - ${item.UnitPrice}</li>
-            <span>
-                <button>-</button>
-                0
-                <button>+</button>
-            </span>
-        <button>Addutocart</button>
-    </div>
+    products.innerHTML += `
+      <div class="square">
+        <div class="image-container">
+          ${item.Description} - ${item.UnitPrice}
+         </div>
+         <div class="amount-container">
+            <button class='bx bxs-minus-square'></button>
+            <div class="count-container">
+            <input type="number" class="count">
+         </div>
+            <button class='bx bxs-plus-square'></button>
+            </div>
+            <div class="price-container">
+            <button class="price">Add to Cart</button>
+        </div>
+      </div>
     `;
   });
-});
+})();
 
 function checkUser() {
   if (!localStorage.getItem('user')) {
     window.alert('You are not authorized to view this page');
-    window.history.back()
+    window.history.back();
   }
 }
