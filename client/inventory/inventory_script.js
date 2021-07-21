1e355b626e571f55b44ad2f0a35ecbebd273b4c9
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-21 15:33:01 +0800

    display top 5 items per month

--- client/inventory/inventory_script.js
+++ client/inventory/inventory_script.js
@@ -1,39 +1,54 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-let logoutButton = document.getElementById("logout");
+let sidebar = document.querySelector('.sidebar');
+let closeBtn = document.querySelector('#btn');
+let searchBtn = document.querySelector('.bx-search');
+let logoutButton = document.getElementById('logout');
+let itemDemandDiv = document.getElementById('item-demand');
 
-logoutButton.addEventListener("click", () => {
-  window.location.href = "http://localhost:5000/";
-  localStorage.removeItem("user");
+logoutButton.addEventListener('click', () => {
+  window.location.href = 'http://localhost:5000/';
+  localStorage.removeItem('user');
 });
 
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
-
-searchBtn.addEventListener("click", () => {
-  // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
   menuBtnChange(); //calling the function(optional)
 });
 
 function menuBtnChange() {
-  if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+  if (sidebar.classList.contains('open')) {
+    closeBtn.classList.replace('bx-menu', 'bx-menu-alt-right'); //replacing the iocns class
   } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+    closeBtn.classList.replace('bx-menu-alt-right', 'bx-menu'); //replacing the iocns class
   }
 }
 
 function checkUser() {
   if (
-    !localStorage.getItem("user") ||
-    JSON.parse(localStorage.getItem("user")).Role === "Customer" ||
-    JSON.parse(localStorage.getItem("user")).Role === "InventoryManager"
+    !localStorage.getItem('user') ||
+    JSON.parse(localStorage.getItem('user')).Role === 'Customer' ||
+    JSON.parse(localStorage.getItem('user')).Role === 'InventoryManager'
   ) {
-    window.alert("You are not authorized to view this page");
-    window.location.href = "http://localhost:5000/";
+    window.alert('You are not authorized to view this page');
+    window.location.href = 'http://localhost:5000/';
   }
 }
+
+(async function getItemDemand() {
+  let response = await fetch('http://localhost:5000/retail/monthly_demand');
+  let json = await response.json();
+  json.forEach((item, idx) => {
+    if (item.Description === null || item.Description === '?') {
+      item.Description = 'Unnamed Item';
+    }
+    if (idx % 5 === 0) {
+      itemDemandDiv.innerHTML += `<h3>${item.Month.split('-')[0]}-${
+        item.Month.split('-')[1]
+      }</h3>`;
+    }
+    itemDemandDiv.innerHTML += `
+     <div>
+      ${item.MonthlyRank}.) ${item.Description} - ${item.Quantity} sales
+     </div> 
+    `;
+  });
+})();
