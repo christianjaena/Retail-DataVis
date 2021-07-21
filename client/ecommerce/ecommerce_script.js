7611c251ffa1480b95b93dde0d6ab64b1d91ee03
christianjaena <christian.jaena@msugensan.edu.ph>
2021-07-21 16:44:00 +0800

    basic functionality of ecommerce

--- client/ecommerce/ecommerce_script.js
+++ client/ecommerce/ecommerce_script.js
@@ -14,22 +14,50 @@ logoutButton.addEventListener('click', () => {
 
   data.forEach((item, idx) => {
     products.innerHTML += `
-      <div class="square">
+      <div id="${idx}" class="square">
         <div class="image-container">
-          ${item.Description} - ${item.UnitPrice}
-         </div>
-         <div class="amount-container">
+          <p>${item.Description}</p>
+          <p>${item.UnitPrice}</p>
+        </div>
+        <div class="amount-container">
             <button class='bx bxs-minus-square'></button>
             <div class="count-container">
-            <input type="number" class="count">
-         </div>
-            <button class='bx bxs-plus-square'></button>
+              <input type="number" class="count" value="1" min="1">
             </div>
-            <div class="price-container">
+            <button class='bx bxs-plus-square'></button>
+        </div>
+        <div class="price-container">
             <button class="price">Add to Cart</button>
         </div>
       </div>
     `;
+  });
+  let itemDiv = document.querySelectorAll('.square');
+  itemDiv.forEach((item) => {
+    let imageContainer = item.children[0];
+    let description = imageContainer.children[0].innerHTML;
+    let price = parseFloat(imageContainer.children[1].innerHTML);
+    let amountContainer = item.children[1];
+    let addToCartButton = item.children[2].children[0];
+    let decrementButton = amountContainer.children[0];
+    let quantity = amountContainer.children[1].children[0];
+    let incrementButton = amountContainer.children[2];
+
+    decrementButton.addEventListener('click', () => {
+      if (quantity.value > 1) {
+        quantity.value = parseInt(quantity.value) - 1;
+      }
+    });
+
+    incrementButton.addEventListener('click', () => {
+      quantity.value = parseInt(quantity.value) + 1;
+    });
+
+    addToCartButton.addEventListener('click', () => {
+      console.log(description);
+      console.log(price * quantity.value);
+      quantity.value = 1;
+    });
   });
 })();
 
